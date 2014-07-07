d2b600083a3881c1567e03aa93eff4de121b5f1b
Hajin <zuharu@gmail.com>
2014-07-07 14:32:21 +0900

    Start 14-07-07

---
+++ OneLevelHero-core/src/com/mygdx/test/OneLevelHeroTest.java
@@ -0,0 +1,18 @@
+package com.mygdx.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import com.mygdx.game.*;
+public class OneLevelHeroTest {
+	OneLevelHero oneLevelHero;
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void plus(){
+		assertEquals(3,oneLevelHero.plus(1, 2));
+	}
+
+}
