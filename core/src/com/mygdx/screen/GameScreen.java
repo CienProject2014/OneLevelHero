f3b7ed0ffb5c6f743eb6c96e6ba73a9a40d3fe5f
NakOh <kk0704@naver.com>
2014-07-07 17:32:06 +0900

    #2 만드는중

--- core/src/com/mygdx/screen/GameScreen.java
+++
@@ -1,5 +0,0 @@
-package com.mygdx.screen;
-
-public class GameScreen {
-
-}
---
+++ core/src/com/mygdx/screens/GameScreen.java
@@ -0,0 +1,48 @@
+package com.mygdx.screens;
+import com.badlogic.gdx.Screen;
+
+public class GameScreen implements Screen {
+
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
