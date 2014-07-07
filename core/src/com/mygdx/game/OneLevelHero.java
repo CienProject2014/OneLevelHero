4cd67abed3d3ce88e44f80ca37468ad01a3e7155
Hajin <zuharu@gmail.com>
2014-07-07 17:42:04 +0900

    마스터 커밋 테스트

--- core/src/com/mygdx/game/OneLevelHero.java
+++ core/src/com/mygdx/game/OneLevelHero.java
@@ -18,6 +18,7 @@ public class OneLevelHero extends ApplicationAdapter {
 
 	@Override
 	public void render () {
+		System.out.println("asdf");
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
