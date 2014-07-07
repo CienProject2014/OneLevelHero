e4ef834f390c2406264b6c49e632bcb68a004079
Hajin <zuharu@gmail.com>
2014-07-07 17:23:21 +0900

    미완성 #1

--- core/src/com/mygdx/game/screens/MenuScreen.java
+++ core/src/com/mygdx/game/screens/MenuScreen.java
@@ -1,13 +1,40 @@
 package com.mygdx.game.screens;
 
+
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.mygdx.game.*;
 
 public class MenuScreen implements Screen {
+
+	OneLevelHero game;
+	Stage stage;
+	TextButton startButton;
+	TextButton optionsButton;
+	TextButton exitButton;
+	
+	public MenuScreen(OneLevelHero game) {
+		this.game = game;
+	}
 
 	@Override
 	public void render(float delta) {
 		// TODO Auto-generated method stub
-		
+		System.out.println("Menu");
+		Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		stage.draw();
 	}
 
 	@Override
@@ -19,7 +46,29 @@ public class MenuScreen implements Screen {
 	@Override
 	public void show() {
 		// TODO Auto-generated method stub
+		stage = new Stage();
+		Skin skin = new Skin();
 		
+		startButton = new TextButton("New Game", skin);
+		startButton.setX(Gdx.graphics.getWidth());
+		startButton.setY(Gdx.graphics.getHeight());
+		startButton.setWidth(100);
+		startButton.setHeight(50);
+		
+		stage.addActor(startButton);
+		
+		startButton.addListener(new InputListener() {
+
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y,
+					int pointer, int button) {
+				// TODO Auto-generated method stub
+				game.setScreen(new GameScreen(game));
+				
+				return true;
+			}
+			
+		});
 	}
 
 	@Override
