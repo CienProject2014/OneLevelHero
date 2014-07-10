960a1067b73110e4a1fcc79f46a332ccb753a36f
dragond7 <dragond7@naver.com>
2014-07-10 18:23:53 +0900

    #7 println대신 gdx 로깅 사용

--- core/src/com/mygdx/screen/PrologueScreen.java
+++ core/src/com/mygdx/screen/PrologueScreen.java
@@ -28,7 +28,7 @@ public class PrologueScreen implements Screen{
 		// TODO Auto-generated method stub
 		Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		System.out.println("Prologue");
+		Gdx.app.log("Prologue Message", "Prologue");
 		
 		stage.draw();
 	}
