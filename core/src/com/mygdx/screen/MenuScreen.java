ef4dd24c9cece88f333554750ccd0c7c66279886
NakOh <kk0704@naver.com>
2014-07-11 14:20:29 +0900

    Last Setting

--- core/src/com/mygdx/screen/MenuScreen.java
+++ core/src/com/mygdx/screen/MenuScreen.java
@@ -72,7 +72,7 @@ public class MenuScreen implements Screen {
 			@Override
 			public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
 				dispose();
-				game.setScreen(new GameScreen(game));
+				game.setScreen(new LoadScreen(game));
 			}
 		});
 		optionsButton.addListener(new InputListener() {
