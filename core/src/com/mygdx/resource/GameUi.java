4c9f2e7011019ba8cb93a02090234b7c3db4ad0e
NakOh <kk0704@naver.com>
2014-08-08 12:28:25 +0900

    #34 kong이 만든 틀에서 구체화작업한 것 옮기는 중

--- core/src/com/mygdx/resource/GameUi.java
+++ core/src/com/mygdx/resource/GameUi.java
@@ -6,14 +6,19 @@ import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.mygdx.game.OneLevelHero;
 import com.mygdx.screen.BattleScreen;
 
 public class GameUi extends Table {
 	OneLevelHero game;
+	ImageButton downArrowButton;
+	ImageButton bagButton;
+	ImageButton helpButton;
 	ImageButton optionButton;
-	ImageButton minimapButton;
-	ImageButton inventoryButton;
+	TextButton worldMapButton;
+	TextButton leftTimeButton;
+
 	TextButton statusButton1;
 	TextButton statusButton2;
 	TextButton statusButton3;
@@ -34,9 +39,16 @@ public class GameUi extends Table {
 		realheight = Assets.realHeight;
 		realwidth = Assets.realWidth;
 
-		optionButton = new ImageButton(Assets.ibuttond, Assets.ibuttonu);
-		minimapButton = new ImageButton(Assets.ibuttond, Assets.ibuttonu);
-		inventoryButton = new ImageButton(Assets.ibuttond, Assets.ibuttonu);
+		TextButtonStyle style = new TextButtonStyle(Assets.nameAndTime,
+				Assets.nameAndTime, Assets.nameAndTime, Assets.font);
+		downArrowButton = new ImageButton(Assets.downArrowButton,
+				Assets.downArrowButton);
+		bagButton = new ImageButton(Assets.bagButton, Assets.bagButton);
+		worldMapButton = new TextButton("worldMap", style);
+		leftTimeButton = new TextButton("12h30m", style);
+		helpButton = new ImageButton(Assets.helpButton, Assets.helpButton);
+		optionButton = new ImageButton(Assets.optionButton, Assets.optionButton);
+
 		statusButton1 = new TextButton("status1", Assets.skin);
 		statusButton2 = new TextButton("status2", Assets.skin);
 		statusButton3 = new TextButton("status3", Assets.skin);
@@ -51,10 +63,19 @@ public class GameUi extends Table {
 
 		this.setFillParent(true);
 
-		toptable.add(minimapButton).expand().width(100).height(50).top().left();
-		toptable.add(inventoryButton).width(100).height(50).top().left();
-		toptable.add(optionButton).width(100).height(50).top().left();
-		toptable.add(battleButton).width(100).height(50).top().right();
+		toptable.add(downArrowButton).expand().width(realwidth / 8)
+				.height(realwidth / 12).top().left();
+		toptable.add(bagButton).width(realwidth / 8).height(realwidth / 12)
+				.top();
+		toptable.add(worldMapButton).width(realwidth / 4)
+				.height(realwidth / 12).top();
+		toptable.add(leftTimeButton).width(realwidth / 4)
+				.height(realwidth / 12).top();
+		toptable.add(helpButton).width(realwidth / 8).height(realwidth / 12)
+				.top().left();
+		toptable.add(optionButton).width(realwidth / 8).height(realwidth / 12)
+				.top();
+		// toptable.add(battleButton).width(100).height(realwidth / 12).top();
 
 		buttomtable.add(statusButton1).width(realwidth / 3)
 				.height(realheight / 7).bottom().left();
