237b06e3f74ee0734b1e4d0e452f0fc0f524323b
HAJIN <zuharu@gmail.com>
2014-08-20 13:44:11 +0900

    Revert "#54 AndroidLauncher.java에 goImmersive함수 추가. 실행"
    This reverts commit 75fd9e7d6d0ac492d6c92c1b4c89a95fab0b4b74.

--- android/src/com/mygdx/game/android/AndroidLauncher.java
+++ android/src/com/mygdx/game/android/AndroidLauncher.java
@@ -1,7 +1,6 @@
 package com.mygdx.game.android;
 
 import android.os.Bundle;
-import android.view.View;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
@@ -13,17 +12,5 @@ public class AndroidLauncher extends AndroidApplication {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new OneLevelHero(), config);
-		
-		goImmersive();
-	}
-	
-	void goImmersive() {
-		getWindow().getDecorView().setSystemUiVisibility(
-            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 	}
 }
