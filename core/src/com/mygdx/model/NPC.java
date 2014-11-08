3d05d3ce1e51156d548e8de04282d9fced1d4eed
dragond7 <dragond7@naver.com>
2014-11-08 11:25:54 +0900

    #82 NPC클래스에 equals와 hashcode메서드를 오버라이딩함

--- core/src/com/mygdx/model/NPC.java
+++ core/src/com/mygdx/model/NPC.java
@@ -6,6 +6,38 @@ public class NPC extends Unit {
 	private List<Event> event;
 	private Event greeting;
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((event == null) ? 0 : event.hashCode());
+		result = prime * result
+				+ ((greeting == null) ? 0 : greeting.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		NPC other = (NPC) obj;
+		if (event == null) {
+			if (other.event != null)
+				return false;
+		} else if (!event.equals(other.event))
+			return false;
+		if (greeting == null) {
+			if (other.greeting != null)
+				return false;
+		} else if (!greeting.equals(other.greeting))
+			return false;
+		return true;
+	}
+
 	public List<Event> getEvent() {
 		return event;
 	}
