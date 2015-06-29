7713480b6c94470a5b4bc1b446f6af577a307d77
LMinJae <lee.min.jae.96@gmail.com>
2015-06-29 20:16:16 +0900

    #170 던전내 이용
    move함수는 array가 표시되지 않은 경우에 대한 예외 처리 X.
    내부 탐색 O, 외부로의 탈출 X
    던전 맵에 맟추어 수정 필요.

--- core/src/com/mygdx/stage/DungeonStage.java
+++ core/src/com/mygdx/stage/DungeonStage.java
@@ -1,14 +1,10 @@
 package com.mygdx.stage;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
-import com.mygdx.currentState.PositionInfo;
-import com.mygdx.enums.ScreenEnum;
 import com.mygdx.manager.CameraManager.CameraPosition;
 import com.mygdx.state.StaticAssets;
 import com.uwsoft.editor.renderer.actor.CompositeItem;
@@ -18,15 +14,30 @@ import com.uwsoft.editor.renderer.actor.CompositeItem;
  *
  */
 public class DungeonStage extends OverlapStage {
-	@Autowired
-	private PositionInfo positionInfo; // 나중에 쓸거임 지우지 마셈
 	private CompositeItem arrowUp, arrowDown, arrowLeft, arrowRight;
+
+	private final static int dungeonMapFlag_Disabled = 0;
+	private final static int dungeonMapFlag_Dir_N = 1 << 1;
+	private final static int dungeonMapFlag_Dir_E = 1 << 2;
+	private final static int dungeonMapFlag_Dir_S = 1 << 3;
+	private final static int dungeonMapFlag_Dir_W = 1 << 4;
+
+	private final static int Dir_N = 0 << 1 | 0 << 0;
+	private final static int Dir_E = 0 << 1 | 1 << 0;
+	private final static int Dir_S = 1 << 1 | 0 << 0;
+	private final static int Dir_W = 1 << 1 | 1 << 0;
+
+	private int[][] map_data;
+	private boolean[][] map_visible;
+	private int sizeX, sizeY, currentX, currentY, currentDir;
 
 	public Stage makeStage() {
 		initSceneLoader(StaticAssets.rm);
 
 		makeScene();
 		setButton();
+
+		initMapData();
 
 		return this;
 	}
@@ -44,7 +55,6 @@ public class DungeonStage extends OverlapStage {
 		arrowLeft = sceneLoader.getRoot().getCompositeById("arrow_left");
 		arrowRight = sceneLoader.getRoot().getCompositeById("arrow_right");
 
-		arrowUp.setTouchable(Touchable.enabled);
 		arrowUp.addListener(new InputListener() {
 			@Override
 			public boolean touchDown(InputEvent event, float x, float y,
@@ -56,10 +66,11 @@ public class DungeonStage extends OverlapStage {
 			public void touchUp(InputEvent event, float x, float y,
 					int pointer, int button) {
 				Gdx.app.debug("DungeonStage", "앞으로 이동");
+				moveForward();
+				update();
 			}
 		});
 
-		arrowDown.setTouchable(Touchable.enabled);
 		arrowDown.addListener(new InputListener() {
 			@Override
 			public boolean touchDown(InputEvent event, float x, float y,
@@ -71,11 +82,11 @@ public class DungeonStage extends OverlapStage {
 			public void touchUp(InputEvent event, float x, float y,
 					int pointer, int button) {
 				Gdx.app.debug("DungeonStage", "뒤로 이동");
-				screenFactory.show(ScreenEnum.DUNGEON_ENTRANCE);
+				moveBackward();
+				update();
 			}
 		});
 
-		arrowLeft.setTouchable(Touchable.enabled);
 		arrowLeft.addListener(new InputListener() {
 			@Override
 			public boolean touchDown(InputEvent event, float x, float y,
@@ -87,10 +98,11 @@ public class DungeonStage extends OverlapStage {
 			public void touchUp(InputEvent event, float x, float y,
 					int pointer, int button) {
 				Gdx.app.debug("DungeonStage", "왼쪽으로 이동");
+				moveLeft();
+				update();
 			}
 		});
 
-		arrowRight.setTouchable(Touchable.enabled);
 		arrowRight.addListener(new InputListener() {
 			@Override
 			public boolean touchDown(InputEvent event, float x, float y,
@@ -102,15 +114,107 @@ public class DungeonStage extends OverlapStage {
 			public void touchUp(InputEvent event, float x, float y,
 					int pointer, int button) {
 				Gdx.app.debug("DungeonStage", "오른쪽으로 이동");
+				moveRight();
+				update();
 			}
 		});
 	}
 
-	public PositionInfo getPositionInfo() {
-		return positionInfo;
+	private void initMapData() { // TODO, FIXME
+		sizeX = sizeY = 3;
+		currentX = currentY = currentDir = 0;
+
+		map_data = new int[sizeY][sizeX];
+		map_visible = new boolean[sizeY][sizeX];
+
+		for (int y = 0; y < sizeY; y++)
+			for (int x = 0; x < sizeX; x++) {
+				map_data[y][x] = dungeonMapFlag_Dir_N | dungeonMapFlag_Dir_E
+						| dungeonMapFlag_Dir_S | dungeonMapFlag_Dir_W;
+				map_visible[y][x] = false;
+			}
+
+		update();
 	}
 
-	public void setPositionInfo(PositionInfo positionInfo) {
-		this.positionInfo = positionInfo;
+	private boolean chkMapFlag(int x, int y, int flag) {
+		return (map_data[y][x] & flag) != dungeonMapFlag_Disabled;
+	}
+
+	// N : 00 E : 01 S : 10 W : 11
+
+	private void update() {
+		map_visible[currentY][currentX] = true;
+
+		boolean N = currentY > 0
+				&& chkMapFlag(currentX, currentY, dungeonMapFlag_Dir_N), E = currentX < sizeX - 1
+				&& chkMapFlag(currentX, currentY, dungeonMapFlag_Dir_E), S = currentY < sizeY - 1
+				&& chkMapFlag(currentX, currentY, dungeonMapFlag_Dir_S), W = currentX > 0
+				&& chkMapFlag(currentX, currentY, dungeonMapFlag_Dir_W);
+
+		arrowUp.setVisible((currentDir & (1 << 0)) == 0 ? ((currentDir & (1 << 1)) == 0 ? N
+				: S)
+				: (currentDir & (1 << 1)) == 0 ? E : W);
+		arrowDown
+				.setVisible((currentDir & (1 << 0)) == 0 ? ((currentDir & (1 << 1)) != 0 ? N
+						: S)
+						: (currentDir & (1 << 1)) != 0 ? E : W);
+		arrowLeft
+				.setVisible((currentDir & (1 << 0)) != 0 ? ((currentDir & (1 << 1)) == 0 ? N
+						: S)
+						: (currentDir & (1 << 1)) != 0 ? E : W);
+		arrowRight
+				.setVisible((currentDir & (1 << 0)) != 0 ? ((currentDir & (1 << 1)) != 0 ? N
+						: S)
+						: (currentDir & (1 << 1)) == 0 ? E : W);
+
+		arrowUp.setTouchable(arrowUp.isVisible() ? Touchable.enabled
+				: Touchable.disabled);
+		arrowDown.setTouchable(arrowDown.isVisible() ? Touchable.enabled
+				: Touchable.disabled);
+		arrowLeft.setTouchable(arrowLeft.isVisible() ? Touchable.enabled
+				: Touchable.disabled);
+		arrowRight.setTouchable(arrowRight.isVisible() ? Touchable.enabled
+				: Touchable.disabled);
+	}
+
+	private void tmpmove(boolean flg0, boolean flg1, boolean flg2) {
+		if (flg0) {
+			if (flg1) {
+				currentDir = Dir_N;
+				currentY -= 1;
+			} else {
+				currentDir = Dir_S;
+				currentY += 1;
+			}
+		} else {
+			if (flg2) {
+				currentDir = Dir_E;
+				currentX += 1;
+			} else {
+				currentDir = Dir_W;
+				currentX -= 1;
+			}
+		}
+	}
+
+	private void moveForward() {
+		tmpmove(((currentDir & (1 << 0)) == 0), ((currentDir & (1 << 1)) == 0),
+				((currentDir & (1 << 1)) == 0));
+	}
+
+	private void moveBackward() {
+		tmpmove(((currentDir & (1 << 0)) == 0), ((currentDir & (1 << 1)) != 0),
+				((currentDir & (1 << 1)) != 0));
+	}
+
+	private void moveLeft() {
+		tmpmove(((currentDir & (1 << 0)) != 0), ((currentDir & (1 << 1)) == 0),
+				((currentDir & (1 << 1)) != 0));
+	}
+
+	private void moveRight() {
+		tmpmove(((currentDir & (1 << 0)) != 0), ((currentDir & (1 << 1)) != 0),
+				((currentDir & (1 << 1)) == 0));
 	}
 }
