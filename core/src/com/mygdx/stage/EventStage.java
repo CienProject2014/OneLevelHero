90fe46de4669c8d68099a41b3030ffaa9809c5f9
ChoiJIN <zuharu@gmail.com>
2015-07-15 21:30:19 +0900

    #181 다른 스테이지 정리

--- core/src/com/mygdx/stage/EventStage.java
+++ core/src/com/mygdx/stage/EventStage.java
@@ -1,15 +1,16 @@
 package com.mygdx.stage;
 
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.mygdx.assets.StaticAssets;
 import com.mygdx.assets.UiComponentAssets;
 import com.mygdx.currentState.EventInfo;
-import com.mygdx.enums.EventTypeEnum;
 import com.mygdx.model.EventScene;
 
 /**
@@ -23,7 +24,10 @@ public class EventStage extends BaseOneLevelStage {
 	private UiComponentAssets uiComponentAssets;
 	@Autowired
 	private EventInfo eventInfo;
-	private Label script;
+	private HashMap<String, Float> uiConstantsMap = StaticAssets.uiConstantsMap
+			.get("EventStage");
+	private Label scriptTitle;
+	private Label scriptContent;
 	private Image characterImage;
 	private Image backgroundImage;
 
@@ -36,70 +40,44 @@ public class EventStage extends BaseOneLevelStage {
 	public Stage makeStage(EventScene eventScene) {
 		super.makeStage();
 		backgroundImage = new Image(eventScene.getBackground());
-		script = new Label(eventScene.getScript(), uiComponentAssets.getSkin());
+		scriptTitle = new Label("Title", uiComponentAssets.getSkin());
+		scriptContent = new Label(eventScene.getScript(),
+				uiComponentAssets.getSkin());
 		characterImage = new Image(eventScene.getCharacter());
 
-		// 스크립트/캐릭터/백그라운드 크기값 세팅
-		script.setFontScale(StaticAssets.windowWidth / 1280);
-		script.setWrap(true);
-		script.setSize(StaticAssets.windowWidth * 0.781f, StaticAssets.windowHeight * 0.185f);
-		script.setPosition(StaticAssets.windowWidth * 0.309375f, StaticAssets.windowHeight * 0.185f);
-		characterImage.setSize(StaticAssets.windowWidth * 0.250f, StaticAssets.windowHeight * 0.555f);
-		characterImage.setPosition(StaticAssets.windowWidth * 0.375f, StaticAssets.windowHeight * 0.185f);
-		backgroundImage.setSize(StaticAssets.windowWidth, StaticAssets.windowHeight);
-
-		// Greeting인지 아닌지 여부에 따라 처리
-		EventTypeEnum eventType;
-		if (eventInfo.isGreeting())
-			eventType = EventTypeEnum.SELECT;
-		else
-			eventType = eventInfo.getEventType();
-		makeEventStage(eventType);
-		this.addActor(backgroundImage);
-		this.addActor(script);
-		this.addActor(characterImage);
+		tableStack.add(backgroundImage);
+		tableStack.add(makeChatTable());
 
 		return this;
 	}
 
-	private void makeEventStage(EventTypeEnum eventType) {
-		switch (eventType) {
-		case CHAT:
-			makeChatStage();
-			break;
-		case SELECT:
-			makeSelectStage();
-			break;
-		case CREDIT:
-			makeCreditStage();
-			break;
-		default:
-			Gdx.app.log("error", " scene 주입 에러");
-			break;
-		}
-	}
+	private Table makeChatTable() {
+		Table chatTable = new Table();
+		chatTable.left().bottom();
 
-	private void makeCreditStage() {
-	}
+		chatTable.add(characterImage).width(uiConstantsMap.get("talkerWidth"))
+				.height(uiConstantsMap.get("talkerHeight"))
+				.padLeft(uiConstantsMap.get("talkerPadLeft"));
 
-	private void makeSelectStage() {
-		script.setFontScale(StaticAssets.windowWidth / 1280);
-		script.setWrap(true); // 스크립트가 끝에 다다르면 자동 개행
-		script.setSize(StaticAssets.windowWidth * 0.781f, StaticAssets.windowHeight * 0.185f);
-		script.setPosition(StaticAssets.windowWidth * 0.109375f, StaticAssets.windowHeight * 0.185f);
-		characterImage.setSize(StaticAssets.windowWidth * 0.250f, StaticAssets.windowHeight * 0.555f);
-		characterImage.setPosition(StaticAssets.windowWidth * 0.375f, StaticAssets.windowHeight * 0.37f);
-		backgroundImage.setSize(StaticAssets.windowWidth, StaticAssets.windowHeight);
-	}
+		scriptContent.setFontScale(1.0f);
+		scriptContent.setWrap(true);
+		scriptContent.setSize(uiConstantsMap.get("scriptWidth"),
+				uiConstantsMap.get("scriptHeight"));
 
-	private void makeChatStage() {
-		script.setFontScale(StaticAssets.windowWidth / 1280);
-		script.setWrap(true);
-		script.setPosition(StaticAssets.windowWidth * 0.2f, 0);
-		script.setSize(StaticAssets.windowWidth * 0.8f, StaticAssets.windowHeight * 0.3f);
-		characterImage.setSize(StaticAssets.windowWidth * 0.2f, StaticAssets.windowHeight * 0.2f);
-		characterImage.setPosition(0, 0);
-		backgroundImage.setSize(StaticAssets.windowWidth, StaticAssets.windowHeight);
+		Table scriptTable = new Table();
+		scriptTable.add(scriptTitle)
+				.width(uiConstantsMap.get("scriptTitleWidth"))
+				.height(uiConstantsMap.get("scriptTitleHeight"))
+				.padBottom(uiConstantsMap.get("scriptTitlePadBottom"));
+		scriptTable.row();
+		scriptTable.add(scriptContent)
+				.width(uiConstantsMap.get("scriptContentWidth"))
+				.height(uiConstantsMap.get("scriptContentHeight"));
+
+		chatTable.add(scriptTable).padLeft(uiConstantsMap.get("scriptPadLeft"))
+				.padBottom(uiConstantsMap.get("scriptPadBottom"));
+
+		return chatTable;
 	}
 
 	public EventInfo getEventInfo() {
