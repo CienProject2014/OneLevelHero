f23c6998ce6f1be9eb0a7f9e89440dc61d9de2fa
HoraceVelmont <dragond7@naver.com>
2015-07-22 21:54:03 +0900

    #228 make arrow button in village Screen

--- core/src/com/mygdx/stage/VillageStage.java
+++ core/src/com/mygdx/stage/VillageStage.java
@@ -1,5 +1,8 @@
 package com.mygdx.stage;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,12 +16,15 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.mygdx.assets.StaticAssets;
 import com.mygdx.assets.UiComponentAssets;
+import com.mygdx.assets.WorldMapAssets;
 import com.mygdx.assets.WorldNodeAssets;
 import com.mygdx.enums.PlaceEnum;
 import com.mygdx.enums.ScreenEnum;
 import com.mygdx.manager.CameraManager.CameraStateEnum;
 import com.mygdx.manager.StorySectionManager;
+import com.mygdx.manager.WorldMapManager;
 import com.mygdx.model.Building;
+import com.mygdx.model.Connection;
 import com.mygdx.model.Village;
 import com.uwsoft.editor.renderer.actor.CompositeItem;
 
@@ -29,6 +35,10 @@ public class VillageStage extends BaseOverlapStage {
 	private UiComponentAssets uiComponentAssets;
 	@Autowired
 	private StorySectionManager storySectionManager;
+	@Autowired
+	private WorldMapAssets worldMapAssets;
+	@Autowired
+	private WorldMapManager worldMapManager;
 	private Village villageInfo;
 	public TextButton shiftButton;
 	private final int movingSpeed = 10;
@@ -43,14 +53,47 @@ public class VillageStage extends BaseOverlapStage {
 		return this;
 	}
 
+	private void setArrow() {
+		List<CompositeItem> arrowList = new ArrayList<CompositeItem>();
+		String currentNode = positionManager.getCurrentNode();
+		Map<String, Connection> connectionMap = worldMapAssets
+				.getWorldNodeInfo(currentNode).getConnection();
+		for (final Entry<String, Connection> connection : connectionMap
+				.entrySet()) {
+			final CompositeItem arrow = sceneLoader.getRoot().getCompositeById(
+					connection.getValue().getArrowName());
+			if (arrow != null) {
+				arrow.setVisible(true);
+				arrow.setTouchable(Touchable.enabled);
+				arrow.addListener(new InputListener() {
+					@Override
+					public boolean touchDown(InputEvent event, float x,
+							float y, int pointer, int button) {
+						return true;
+					}
+
+					@Override
+					public void touchUp(InputEvent event, float x, float y,
+							int pointer, int button) {
+						worldMapManager.selectDestinationNode(connection
+								.getKey());
+						screenFactory.show(ScreenEnum.MOVING);
+					}
+				});
+				arrowList.add(arrow);
+			}
+		}
+	}
+
 	// 마을 정보에 맞게 스테이지 형성
 	private void setVillage() {
 		Gdx.app.debug("VillageStage",
 				String.valueOf(positionManager.getCurrentNode()));
 		villageInfo = worldNodeAssets.getVillage("blackwood");
 		sceneLoader.loadScene("blackwood_scene");
-		addActor(sceneLoader.getRoot());
+		setArrow();
 		setBuildingButton();
+		addActor(sceneLoader.getRoot());
 		final CompositeItem shiftbutton_up = sceneLoader.getRoot()
 				.getCompositeById("camera_up");
 		final CompositeItem shiftbutton_down = sceneLoader.getRoot()
