f128d6baf35f772fa1b58412935de8b16012bffa
ChoiJIN <zuharu@gmail.com>
2015-07-23 19:03:15 +0900

    #NO-ISSUE grid hitbox hotfix for demo

--- core/src/com/mygdx/stage/BattleStage.java
+++ core/src/com/mygdx/stage/BattleStage.java
@@ -40,8 +40,7 @@ public class BattleStage extends BaseOneLevelStage {
 	private final String NORMAL_ATTACK = "normal_attack";
 	private final String SKILL_ATTACK = "skill_attack";
 
-	private HashMap<String, Float> uiConstantsMap = StaticAssets.uiConstantsMap
-			.get("BattleStage");
+	private HashMap<String, Float> uiConstantsMap = StaticAssets.uiConstantsMap.get("BattleStage");
 	// Table
 	private Table orderTable; // 순서를 나타내는 테이블
 	private GridHitbox gridHitbox; // grid hitbox 테이블
@@ -146,8 +145,7 @@ public class BattleStage extends BaseOneLevelStage {
 		Table RMenuTable = makeRMenuTable();
 
 		uiTable.right().bottom();
-		uiTable.padRight(uiConstantsMap.get("RMenuTablePadRight")).padBottom(
-				uiConstantsMap.get("RMenuTablePadBottom"));
+		uiTable.padRight(uiConstantsMap.get("RMenuTablePadRight")).padBottom(uiConstantsMap.get("RMenuTablePadBottom"));
 		uiTable.add(RMenuTable);
 
 		return uiTable;
@@ -158,25 +156,19 @@ public class BattleStage extends BaseOneLevelStage {
 		makeRButton();
 
 		rMenuTable.add(attackButton).width(uiConstantsMap.get("RButtonWidth"))
-				.height(uiConstantsMap.get("RButtonHeight"))
-				.padTop(uiConstantsMap.get("RMenuTablePadTop"))
+				.height(uiConstantsMap.get("RButtonHeight")).padTop(uiConstantsMap.get("RMenuTablePadTop"))
 				.padBottom(uiConstantsMap.get("RButtonSpace")).expandX();
 		rMenuTable.row();
 		rMenuTable.add(skillButton).width(uiConstantsMap.get("RButtonWidth"))
-				.height(uiConstantsMap.get("RButtonHeight"))
-				.padBottom(uiConstantsMap.get("RButtonSpace"));
+				.height(uiConstantsMap.get("RButtonHeight")).padBottom(uiConstantsMap.get("RButtonSpace"));
 		rMenuTable.row();
-		rMenuTable.add(inventoryButton)
-				.width(uiConstantsMap.get("RButtonWidth"))
-				.height(uiConstantsMap.get("RButtonHeight"))
-				.padBottom(uiConstantsMap.get("RButtonSpace"));
+		rMenuTable.add(inventoryButton).width(uiConstantsMap.get("RButtonWidth"))
+				.height(uiConstantsMap.get("RButtonHeight")).padBottom(uiConstantsMap.get("RButtonSpace"));
 		rMenuTable.row();
 		rMenuTable.add(defenseButton).width(uiConstantsMap.get("RButtonWidth"))
-				.height(uiConstantsMap.get("RButtonHeight"))
-				.padBottom(uiConstantsMap.get("RButtonSpace"));
+				.height(uiConstantsMap.get("RButtonHeight")).padBottom(uiConstantsMap.get("RButtonSpace"));
 		rMenuTable.row();
-		rMenuTable.add(waitButton).width(uiConstantsMap.get("RButtonWidth"))
-				.height(uiConstantsMap.get("RButtonHeight"))
+		rMenuTable.add(waitButton).width(uiConstantsMap.get("RButtonWidth")).height(uiConstantsMap.get("RButtonHeight"))
 				.padBottom(uiConstantsMap.get("RButtonSpace"));
 		rMenuTable.row();
 		rMenuTable.add(escapeButton).width(uiConstantsMap.get("RButtonWidth"))
@@ -199,9 +191,8 @@ public class BattleStage extends BaseOneLevelStage {
 	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
 		boolean result = super.touchDown(screenX, screenY, pointer, button);
 
-		if (gridHitbox.isGridShow()
-				&& gridHitbox.isInsideHitbox(touched.x, touched.y)) {
-			gridHitbox.showTileWhereClicked(screenX, screenY);
+		if (gridHitbox.isGridShow() && gridHitbox.isInsideHitbox(touched.x, touched.y)) {
+			gridHitbox.showTileWhereClicked(touched.x, touched.y);
 		}
 		return result;
 	}
@@ -210,7 +201,6 @@ public class BattleStage extends BaseOneLevelStage {
 	public boolean touchDragged(int screenX, int screenY, int pointer) {
 		boolean result = super.touchDragged(screenX, screenY, pointer);
 
-		Gdx.app.log("deb", touched.toString());
 		if (gridHitbox.isGridShow()) {
 			gridHitbox.showTileWhereMoved(touched.x, touched.y);
 		}
@@ -221,8 +211,7 @@ public class BattleStage extends BaseOneLevelStage {
 	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
 		boolean result = super.touchUp(screenX, screenY, pointer, button);
 
-		if (gridHitbox.isGridShow()
-				&& gridHitbox.isInsideHitbox(touched.x, touched.y)) {
+		if (gridHitbox.isGridShow() && gridHitbox.isInsideHitbox(touched.x, touched.y)) {
 			Gdx.app.log("BattleStage", "어택!");
 
 			Unit actor = getCurrentActor();
@@ -317,23 +306,17 @@ public class BattleStage extends BaseOneLevelStage {
 
 	private void makeRButton() {
 		// 이미지 추가
-		attackButton = new ImageButton(
-				atlasUiAssets.getAtlasUiFile("battleui_rb_attack"),
+		attackButton = new ImageButton(atlasUiAssets.getAtlasUiFile("battleui_rb_attack"),
 				atlasUiAssets.getAtlasUiFile("battleui_rbac_attack"));
-		skillButton = new ImageButton(
-				atlasUiAssets.getAtlasUiFile("battleui_rb_skill"),
+		skillButton = new ImageButton(atlasUiAssets.getAtlasUiFile("battleui_rb_skill"),
 				atlasUiAssets.getAtlasUiFile("battleui_rbac_skill"));
-		inventoryButton = new ImageButton(
-				atlasUiAssets.getAtlasUiFile("battleui_rb_item"),
+		inventoryButton = new ImageButton(atlasUiAssets.getAtlasUiFile("battleui_rb_item"),
 				atlasUiAssets.getAtlasUiFile("battleui_rbac_item"));
-		defenseButton = new ImageButton(
-				atlasUiAssets.getAtlasUiFile("battleui_rb_defense"),
+		defenseButton = new ImageButton(atlasUiAssets.getAtlasUiFile("battleui_rb_defense"),
 				atlasUiAssets.getAtlasUiFile("battleui_rbac_defense"));
-		waitButton = new ImageButton(
-				atlasUiAssets.getAtlasUiFile("battleui_rb_wait"),
+		waitButton = new ImageButton(atlasUiAssets.getAtlasUiFile("battleui_rb_wait"),
 				atlasUiAssets.getAtlasUiFile("battleui_rbac_wait"));
-		escapeButton = new ImageButton(
-				atlasUiAssets.getAtlasUiFile("battleui_rb_escape"),
+		escapeButton = new ImageButton(atlasUiAssets.getAtlasUiFile("battleui_rb_escape"),
 				atlasUiAssets.getAtlasUiFile("battleui_rbac_escape"));
 
 		addListener();
