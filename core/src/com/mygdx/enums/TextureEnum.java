c1487081459c82af2f00f724f07ffd7dcd91b6e8
HoraceVelmont <dragond7@naver.com>
2015-08-10 20:46:53 +0900

    #no-issue bug-fix

--- core/src/com/mygdx/enums/TextureEnum.java
+++ core/src/com/mygdx/enums/TextureEnum.java
@@ -3,7 +3,8 @@ package com.mygdx.enums;
 public enum TextureEnum {
 	BACKGROUND("bg"), FACE("face"), BUST("bust"), NPC("npc"), MONSTER("monster"), NORMAL(
 			"01"), ATTACK_CUTTING("attack_cutting"), ATTACK_CUTTING2(
-			"attack_cutting2"), BATTLE_TURN("battle"), STATUS("status");
+			"attack_cutting2"), BATTLE_TURN("battle"), STATUS("status"), BACKGROUND_UP(
+			"background_up"), BACKGROUND_DOWN("background_down)");
 	// FIXME 애니메이션 시트를 여기에 같이 두는게 적절한가?
 
 	private String textureNumber;
