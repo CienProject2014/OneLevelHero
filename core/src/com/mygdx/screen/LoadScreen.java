95a4f0846f2f656c32d8a2f2d9fad154619edfe9
HoraceVelmont <dragond7@naver.com>
2015-08-24 21:21:01 +0900

    #417 stage 분리

--- core/src/com/mygdx/screen/LoadScreen.java
+++ core/src/com/mygdx/screen/LoadScreen.java
@@ -1,106 +1,41 @@
 package com.mygdx.screen;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputMultiplexer;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.mygdx.assets.UiComponentAssets;
-import com.mygdx.currentState.CurrentInfo;
-import com.mygdx.enums.ScreenEnum;
 import com.mygdx.enums.StageEnum;
-import com.mygdx.manager.LoadManager;
-import com.mygdx.stage.LoadStage;
+import com.mygdx.stage.LoadPopupStage;
 
 public class LoadScreen extends BaseScreen {
-	@Autowired
-	protected CurrentInfo currentInfo;
-	@Autowired
-	private UiComponentAssets uiComponentAssets;
-	@Autowired
-	private LoadManager loadManager;
-	private Stage stage, loadStage;
-	private TextButton loadButton;
-	private TextButton newStartButton;
-	private TextButton backButton;
+	protected static boolean isTouched;
+	private Stage loadStage, loadPopupStage;
 
 	@Override
 	public void render(float delta) {
 		super.render(delta);
 		setInputProcessor();
-		stage.draw();
-		if (LoadStage.isTouched) {
-			loadStage.draw();
+		loadStage.draw();
+		if (LoadScreen.isTouched) {
+			loadPopupStage.draw();
 		}
 	}
 
 	@Override
 	public void show() {
-		stage = new Stage();
-		Table table = new Table(uiComponentAssets.getSkin());
-		backButton = new TextButton("Back", uiComponentAssets.getSkin());
-		loadButton = new TextButton("Load", uiComponentAssets.getSkin());
-		newStartButton = new TextButton("NewStart", uiComponentAssets.getSkin());
-		backButton.addListener(new InputListener() {
-			@Override
-			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-				return true;
-			}
-
-			@Override
-			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-				screenFactory.show(ScreenEnum.MENU);
-			}
-		});
-		loadButton.addListener(new InputListener() {
-			@Override
-			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-				return true;
-			}
-
-			@Override
-			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-
-				LoadStage.isTouched = true;
-			}
-		});
-		newStartButton.addListener(new InputListener() {
-			@Override
-			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-				return true;
-			}
-
-			@Override
-			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-				eventManager.setCurrentEventNpc("prologue");
-				loadManager.loadNewGame();
-			}
-		});
-
-		table.setFillParent(true);
-		table.add(loadButton).expand();
-		table.row();
-		table.add(newStartButton).expand();
-		table.row();
-		table.add(backButton).bottom();
-		stage.addActor(table);
 		loadStage = stageFactory.makeStage(StageEnum.LOAD);
+		loadPopupStage = stageFactory.makeStage(StageEnum.LOAD_POPUP);
 		setInputProcessor();
 
 	}
 
 	private void setInputProcessor() {
 		InputMultiplexer multiplexer = new InputMultiplexer();
-		if (LoadStage.isTouched) {
-			multiplexer.addProcessor(0, loadStage);
-			multiplexer.addProcessor(1, stage);
-		} else {
-			multiplexer.addProcessor(0, stage);
+		if (LoadPopupStage.isTouched) {
+			multiplexer.addProcessor(0, loadPopupStage);
 			multiplexer.addProcessor(1, loadStage);
+		} else {
+			multiplexer.addProcessor(0, loadStage);
+			multiplexer.addProcessor(1, loadPopupStage);
 		}
 		Gdx.input.setInputProcessor(multiplexer);
 
--- core/src/com/mygdx/stage/LoadStage.java
+++ core/src/com/mygdx/stage/LoadStage.java
@@ -2,129 +2,85 @@ package com.mygdx.stage;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.badlogic.gdx.graphics.Camera;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.mygdx.assets.StaticAssets;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.mygdx.assets.AtlasUiAssets;
+import com.mygdx.assets.UiComponentAssets;
 import com.mygdx.currentState.CurrentInfo;
-import com.mygdx.enums.SaveVersion;
-import com.mygdx.enums.WorldNodeEnum;
-import com.mygdx.manager.AssetsManager;
-import com.mygdx.manager.MovingManager;
-import com.mygdx.manager.SaveManager;
-import com.uwsoft.editor.renderer.actor.CompositeItem;
-import com.uwsoft.editor.renderer.actor.ImageItem;
+import com.mygdx.enums.ScreenEnum;
+import com.mygdx.manager.EventManager;
+import com.mygdx.manager.LoadManager;
+import com.mygdx.manager.TextureManager;
 
-public class LoadStage extends BaseOverlapStage {
+public class LoadStage extends BaseOneLevelStage {
 	@Autowired
-	private AssetsManager assetsManager;
+	protected CurrentInfo currentInfo;
 	@Autowired
-	private SaveManager saveManager;
+	private UiComponentAssets uiComponentAssets;
 	@Autowired
-	private CurrentInfo currentInfo;
+	private LoadManager loadManager;
+	private TextButton loadButton;
+	private TextButton newStartButton;
+	private TextButton backButton;
 	@Autowired
-	private MovingManager movingManager;
-	public static boolean isTouched = false;
-	private static final String SCENE_NAME = "load_scene";
-	private Camera cam;
-	private ImageItem save;
-	private CompositeItem closeButton;
-	private CompositeItem background;
-	private CompositeItem save01;
-	private CompositeItem save02;
-	private CompositeItem save03;
+	private EventManager eventManager;
+	@Autowired
+	private AtlasUiAssets atlasUiAssets;
+	@Autowired
+	private TextureManager textureManager;
 
 	public Stage makeStage() {
-		assetsManager.initScene(SCENE_NAME);
-		initSceneLoader(assetsManager.rm);
-		sceneLoader.loadScene(SCENE_NAME);
-		addActor(sceneLoader.getRoot());
-		setCamera();
-		setCompositeItem();
-		setAddListener();
+		super.makeStage();
+		Table table = new Table(uiComponentAssets.getSkin());
+		backButton = new TextButton("Back", uiComponentAssets.getSkin());
+		loadButton = new TextButton("Load", uiComponentAssets.getSkin());
+		newStartButton = new TextButton("NewStart", uiComponentAssets.getSkin());
+		backButton.addListener(new InputListener() {
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+				return true;
+			}
+
+			@Override
+			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+				screenFactory.show(ScreenEnum.MENU);
+			}
+		});
+		loadButton.addListener(new InputListener() {
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+				return true;
+			}
+
+			@Override
+			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+
+				LoadPopupStage.isTouched = true;
+			}
+		});
+		newStartButton.addListener(new InputListener() {
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+				return true;
+			}
+
+			@Override
+			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+				eventManager.setCurrentEventNpc("prologue");
+				loadManager.loadNewGame();
+			}
+		});
+
+		table.setFillParent(true);
+		table.add(loadButton).expand();
+		table.row();
+		table.add(newStartButton).expand();
+		table.row();
+		table.add(backButton).bottom();
+		tableStack.addActor(table);
 		return this;
-	}
-
-	private void setCompositeItem() {
-		background = sceneLoader.getRoot().getCompositeById("background");
-		save01 = sceneLoader.getRoot().getCompositeById("save01");
-		save02 = sceneLoader.getRoot().getCompositeById("save02");
-		save03 = sceneLoader.getRoot().getCompositeById("save03");
-		save = sceneLoader.getRoot().getImageById("save");
-		closeButton = sceneLoader.getRoot().getCompositeById("close_button");
-	}
-
-	private void setAddListener() {
-		save.setVisible(false);
-		background.addListener(new InputListener() {
-			@Override
-			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-				return true;
-			}
-
-			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-				isTouched = false;
-			}
-		});
-		save01.addListener(new InputListener() {
-			@Override
-			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-				return true;
-			}
-
-			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-				currentInfo.setSaveVersion(SaveVersion.SAVE1);
-				saveManager.load();
-				isTouched = false;
-				WorldNodeEnum.NodeType nodeType = positionManager.getCurrentNodeType();
-				movingManager.goCurrentNode(nodeType);
-			}
-		});
-		save02.addListener(new InputListener() {
-			@Override
-			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-				return true;
-			}
-
-			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-				currentInfo.setSaveVersion(SaveVersion.SAVE2);
-				saveManager.load();
-				isTouched = false;
-				WorldNodeEnum.NodeType nodeType = positionManager.getCurrentNodeType();
-				movingManager.goCurrentNode(nodeType);
-			}
-		});
-		save03.addListener(new InputListener() {
-			@Override
-			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-				return true;
-			}
-
-			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-				currentInfo.setSaveVersion(SaveVersion.SAVE3);
-				saveManager.load();
-				isTouched = false;
-				WorldNodeEnum.NodeType nodeType = positionManager.getCurrentNodeType();
-				movingManager.goCurrentNode(nodeType);
-			}
-		});
-		closeButton.addListener(new InputListener() {
-			@Override
-			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-				return true;
-			}
-
-			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-				isTouched = false;
-			}
-		});
-	}
-
-	private void setCamera() {
-		cam = new OrthographicCamera(StaticAssets.BASE_WINDOW_WIDTH, StaticAssets.BASE_WINDOW_HEIGHT);
-		cam.position.set(cam.viewportWidth / 2f, cam.viewportHeight / 2f, 0);
-		getViewport().setCamera(cam);
 	}
 }
