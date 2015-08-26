96804e6f2d39be2f51108980eb28c6268d515fd6
Joe <kong@Joeui-MacBook-Pro.local>
2015-08-26 13:27:07 +0900

    다른 던전 입장 및 엘리트 몹 방 구현

--- core/src/com/mygdx/model/surroundings/Dungeon.java
+++ core/src/com/mygdx/model/surroundings/Dungeon.java
@@ -15,6 +15,7 @@ public class Dungeon {
 	private int mapWidth;
 	private int mapHeight;
 	private boolean inDungeon;
+	private String eliteMonster;
 
 	public ArrayList<DungeonNode> nodes = new ArrayList<>();
 	public ArrayList<DungeonConnection> connections = new ArrayList<>();
@@ -58,5 +59,11 @@ public class Dungeon {
 	}
 	public void setFieldType(FieldTypeEnum fieldType) {
 		this.fieldType = fieldType;
+	}
+	public String getEliteMonster() {
+		return eliteMonster;
+	}
+	public void setEliteMonster(String eliteMonster) {
+		this.eliteMonster = eliteMonster;
 	}
 }
