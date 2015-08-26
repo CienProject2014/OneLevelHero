8a9be48fde537ccef4c679a399735514ed979755
NakOh <kk0704@naver.com>
2015-08-26 16:22:42 +0900

    #466 GameOver적용

--- core/src/com/mygdx/stage/GameOverStage.java
+++ core/src/com/mygdx/stage/GameOverStage.java
@@ -2,45 +2,33 @@ package com.mygdx.stage;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.mygdx.assets.StaticAssets;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.mygdx.enums.ScreenEnum;
-import com.mygdx.factory.ScreenFactory;
-import com.mygdx.manager.SaveManager;
+import com.mygdx.manager.TextureManager;
 
-public class GameOverStage extends Stage {
+public class GameOverStage extends BaseOneLevelStage {
 	@Autowired
-	private ScreenFactory screenFactory;
-	@Autowired
-	private SaveManager saveManager;
+	private TextureManager textureManager;
+	private Table table;
 
 	public Stage makeStage() {
-		Label label = new Label("GAME OVER ... ", StaticAssets.skin);
-		TextButton goToMenuButton = new TextButton("처음 화면으로...", StaticAssets.skin);
-		goToMenuButton.addListener(new ClickListener() {
+		super.makeStage();
+		table = new Table();
+		table.setBackground(
+				new TextureRegionDrawable(new TextureRegion(textureManager.getBackgroundTexture("gameover"))));
+		this.addListener(new ClickListener() {
 			@Override
 			public void clicked(InputEvent event, float x, float y) {
-				initInfo();
-				screenFactory.popAllAndPush(ScreenEnum.MENU);
+				screenFactory.show(ScreenEnum.MENU);
 			}
 		});
-		Table table = new Table();
-		table.setFillParent(true);
-		label.setSize(50, 50);
-		goToMenuButton.setSize(250, 90);
-		table.add(label);
-		table.row();
-		table.add(goToMenuButton);
-		addActor(table);
+		tableStack.add(table);
 		return this;
 	}
 
-	private void initInfo() {
-		saveManager.setNewGame();
-	}
 }
