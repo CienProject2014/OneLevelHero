0d26647f47a001083a28ce5b8d0d96bc8c6862bb
HoraceVelmont <dragond7@naver.com>
2015-09-26 13:59:29 +0900

    #525 OverLap Building 제거

--- core/src/com/mygdx/stage/BuildingStage.java
+++ core/src/com/mygdx/stage/BuildingStage.java
@@ -1,19 +1,12 @@
 package com.mygdx.stage;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
-import com.badlogic.gdx.scenes.scene2d.ui.Stack;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.mygdx.assets.AtlasUiAssets;
@@ -22,13 +15,14 @@ import com.mygdx.assets.EventAssets;
 import com.mygdx.assets.NodeAssets;
 import com.mygdx.assets.StaticAssets;
 import com.mygdx.assets.UiComponentAssets;
-import com.mygdx.enums.GameObjectEnum;
+import com.mygdx.enums.BuildingTypeEnum;
 import com.mygdx.enums.PositionEnum.EventPosition;
 import com.mygdx.enums.ScreenEnum;
 import com.mygdx.factory.ListenerFactory;
 import com.mygdx.listener.SimpleTouchListener;
 import com.mygdx.manager.AssetsManager;
 import com.mygdx.manager.EventManager;
+import com.mygdx.manager.PositionManager;
 import com.mygdx.manager.TextureManager;
 import com.mygdx.manager.TimeManager;
 import com.mygdx.model.event.GameObject;
@@ -37,10 +31,8 @@ import com.mygdx.model.location.Building;
 import com.mygdx.nextSectionChecker.ArgumentChecker;
 import com.mygdx.popup.GameObjectPopup;
 import com.mygdx.screen.BuildingScreen;
-import com.uwsoft.editor.renderer.actor.CompositeItem;
-import com.uwsoft.editor.renderer.actor.LabelItem;
 
-public class BuildingStage extends BaseOverlapStage {
+public class BuildingStage extends BaseOneLevelStage {
 	@Autowired
 	private EventManager eventManager;
 	@Autowired
@@ -61,34 +53,57 @@ public class BuildingStage extends BaseOverlapStage {
 	private ConstantsAssets constantsAssets;
 	@Autowired
 	private TimeManager timeManager;
+	@Autowired
+	private PositionManager positionManager;
 
 	private static final int GAME_OBJECT_POSITION[][] = {{80, 45}, {450, 45}, {820, 45}, {1210, 45}, {1570, 45}};
-	private List<CompositeItem> npcButtonList;
-	private List<CompositeItem> gameObjectList;
 	private Building buildingInfo;
 	private GameObjectPopup gameObjectPopup;
-	protected Stack tableStack;
 
 	public Stage makeStage() {
+		super.makeStage();
 		cameraManager.stretchToDevice(this);
-		tableStack = new Stack();
-		tableStack.setWidth(StaticAssets.BASE_WINDOW_WIDTH);
-		tableStack.setHeight(StaticAssets.BASE_WINDOW_HEIGHT);
 		buildingInfo = nodeAssets.getVillageByPath(positionManager.getCurrentNodePath()).getBuilding()
 				.get(positionManager.getCurrentSubNodePath());
-		if (buildingInfo.isOverlapScene()) {
-			makeScene();
-			makeBuildingSceneByOverlap();
-			setSceneNpcList();
-			setSceneItemList();
-		} else {
-			makeBuildingScene();
-			setNpcList();
-			setGameObject();
-		}
-
+		makeBuildingScene();
+		setBuildingFunction();
+		setNpcList();
+		setGameObject();
 		this.addActor(tableStack);
 		return this;
+	}
+
+	private void setBuildingFunction() {
+		if (buildingInfo.getBuildingType().equals(BuildingTypeEnum.INN)) {
+			Table buttonTable = new Table();
+			ImageButton saveButton = new ImageButton(atlasUiAssets.getAtlasUiFile("stay_button_save"));
+			buttonTable.add(saveButton);
+			buttonTable.row();
+			buttonTable.row();
+
+			saveButton.addListener(new SimpleTouchListener() {
+				public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+					BuildingScreen.isInSave = true;
+				}
+			});
+
+			ImageButton restButton = new ImageButton(new TextureRegionDrawable(new TextureRegion(
+					uiComponentAssets.getStayButton())));
+			buttonTable.add(restButton);
+			buttonTable.row();
+			restButton.addListener(new SimpleTouchListener() {
+				public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+					timeManager.plusMinute(5);
+					gameObjectPopup = new GameObjectPopup();
+					gameObjectPopup.setAtlasUiAssets(atlasUiAssets);
+					gameObjectPopup.setListenerFactory(listenerFactory);
+					gameObjectPopup.setConstantsAssets(constantsAssets);
+					addActor(gameObjectPopup);
+					gameObjectPopup.setVisible(true);
+				}
+			});
+			tableStack.add(buttonTable);
+		}
 	}
 
 	private void setGameObject() {
@@ -102,55 +117,7 @@ public class BuildingStage extends BaseOverlapStage {
 		if (buildingInfo.getGameObject() != null) {
 			for (final String objectName : buildingInfo.getGameObject()) {
 				final GameObject gameObject = eventManager.getEventInfo().getGameObjectMap().get(objectName);
-				if (objectName.equals("save")) {
-					ImageButton saveButton = new ImageButton(atlasUiAssets.getAtlasUiFile("stay_button_save"));
-					buttonTable.add(saveButton);
-					buttonTable.row();
-					buttonTable.row();
-
-					saveButton.addListener(new SimpleTouchListener() {
-						public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-							BuildingScreen.isInSave = true;
-						}
-					});
-				} else if (objectName.equals("rest")) {
-					ImageButton restButton = new ImageButton(new TextureRegionDrawable(new TextureRegion(
-							uiComponentAssets.getStayButton())));
-					buttonTable.add(restButton);
-					buttonTable.row();
-					restButton.addListener(new SimpleTouchListener() {
-						public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-							timeManager.plusMinute(5);
-							eventManager.setCurrentGameObject(gameObject);
-							gameObjectPopup = new GameObjectPopup();
-							gameObjectPopup.setAtlasUiAssets(atlasUiAssets);
-							gameObjectPopup.setListenerFactory(listenerFactory);
-							gameObjectPopup.setGameObject(gameObject);
-							gameObjectPopup.setConstantsAssets(constantsAssets);
-							gameObjectPopup.initialize(objectName);
-							addActor(gameObjectPopup);
-							gameObjectPopup.setVisible(true);
-						}
-					});
-				} else if (objectName.equals("stay")) {
-					ImageButton restsButton = new ImageButton(atlasUiAssets.getAtlasUiFile("stay_button_go"));
-					addActor(restsButton);
-					restsButton.addListener(new SimpleTouchListener() {
-						@Override
-						public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-							timeManager.plusMinute(5);
-							eventManager.setCurrentGameObject(gameObject);
-							gameObjectPopup = new GameObjectPopup();
-							gameObjectPopup.setAtlasUiAssets(atlasUiAssets);
-							gameObjectPopup.setListenerFactory(listenerFactory);
-							gameObjectPopup.setGameObject(gameObject);
-							gameObjectPopup.setConstantsAssets(constantsAssets);
-							gameObjectPopup.initialize(objectName);
-							addActor(gameObjectPopup);
-							gameObjectPopup.setVisible(true);
-						}
-					});
-				} else {
+				{
 					if (gameObject != null) {
 						ImageButton gameObjectButton = new ImageButton(new TextureRegionDrawable(new TextureRegion(
 								textureManager.getGameObjectTexture(gameObject.getFacePath()))));
@@ -173,14 +140,6 @@ public class BuildingStage extends BaseOverlapStage {
 				}
 			}
 		}
-	}
-
-	private void makeScene() {
-		buildingInfo = nodeAssets.getVillageByPath(positionManager.getCurrentNodePath()).getBuilding()
-				.get(positionManager.getCurrentSubNodePath());
-
-		assetsManager.initScene(buildingInfo.getSceneName());
-		initSceneLoader(assetsManager.rm);
 	}
 
 	private void makeBuildingScene() {
@@ -226,104 +185,6 @@ public class BuildingStage extends BaseOverlapStage {
 					}
 				}
 			}
-		}
-	}
-	private void makeBuildingSceneByOverlap() {
-		sceneLoader.loadScene(buildingInfo.getSceneName());
-		addActor(sceneLoader.getRoot());
-	}
-
-	private void setSceneNpcList() {
-		npcButtonList = new ArrayList<CompositeItem>();
-		if (buildingInfo.getBuildingNpc() != null) {
-			for (final String npcName : buildingInfo.getBuildingNpc()) {
-				CompositeItem npcButton = sceneLoader.getRoot().getCompositeById(npcName);
-				npcButton.setTouchable(Touchable.enabled);
-				npcButton.addListener(new SimpleTouchListener() {
-					@Override
-					public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-						timeManager.plusMinute(5);
-						eventManager.setCurrentNpc(npcName);
-						positionManager.setCurrentEventPositionType(EventPosition.NPC);
-						screenFactory.show(ScreenEnum.GREETING);
-					}
-				});
-				npcButtonList.add(npcButton);
-			}
-		}
-	}
-
-	private void setSceneItemList() {
-		gameObjectList = new ArrayList<CompositeItem>();
-
-		if (buildingInfo.getGameObject() != null) {
-			for (final String objectName : buildingInfo.getGameObject()) {
-				final GameObject gameObject = eventAssets.getGameObject(objectName);
-				CompositeItem gameObjectButton = sceneLoader.getRoot().getCompositeById(objectName);
-				gameObjectButton.setVisible(true);
-				setGameObjectVisibility(gameObjectButton, gameObject.getObjectType());
-				setGameObjectFunction(gameObjectButton, objectName);
-				if (objectName.equals("save")) {
-					gameObjectButton.addListener(new SimpleTouchListener() {
-						public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-							BuildingScreen.isInSave = true;
-						}
-					});
-				} else {
-					gameObjectButton.addListener(new SimpleTouchListener() {
-						@Override
-						public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-							timeManager.plusMinute(5);
-							eventManager.setCurrentGameObject(gameObject);
-							gameObjectPopup = new GameObjectPopup();
-							gameObjectPopup.setAtlasUiAssets(atlasUiAssets);
-							gameObjectPopup.setListenerFactory(listenerFactory);
-							gameObjectPopup.setGameObject(gameObject);
-							gameObjectPopup.setConstantsAssets(constantsAssets);
-							gameObjectPopup.initialize(objectName);
-							addActor(gameObjectPopup);
-							gameObjectPopup.setVisible(true);
-						}
-					});
-				}
-			}
-		}
-
-	}
-
-	private void setGameObjectFunction(CompositeItem gameObjectButton, String objectName) {
-		if (objectName.equals("save")) {
-			LabelItem labelItem = sceneLoader.getRoot().getLabelById("save_label");
-			labelItem.setText("저장하기");
-			labelItem.setStyle(new LabelStyle(uiComponentAssets.getFont(), Color.WHITE));
-			labelItem.setFontScale(1.0f);
-			labelItem.setTouchable(Touchable.disabled);
-		} else if (objectName.equals("rest")) {
-			LabelItem labelItem = sceneLoader.getRoot().getLabelById("rest_label");
-			labelItem.setText("휴식하기");
-			labelItem.setStyle(new LabelStyle(uiComponentAssets.getFont(), Color.WHITE));
-			labelItem.setFontScale(1.0f);
-			labelItem.setTouchable(Touchable.disabled);
-		}
-	}
-
-	private void setGameObjectVisibility(CompositeItem objectButton, GameObjectEnum gameObjectEnum) {
-		switch (gameObjectEnum) {
-			case NORMAL :
-				objectButton.setLayerVisibilty(GameObjectEnum.PRESSED.toString(), false);
-				objectButton.setLayerVisibilty(GameObjectEnum.NORMAL.toString(), true);
-				objectButton.setTouchable(Touchable.enabled);
-				break;
-			case PRESSED :
-				objectButton.setLayerVisibilty(GameObjectEnum.PRESSED.toString(), true);
-				objectButton.setLayerVisibilty(GameObjectEnum.NORMAL.toString(), false);
-				objectButton.setTouchable(Touchable.disabled);
-				break;
-			case FUNCTION :
-				break;
-			default :
-				Gdx.app.log("BuildingStage", "NULL GameObjectEnum Type");
-				break;
 		}
 	}
 }
