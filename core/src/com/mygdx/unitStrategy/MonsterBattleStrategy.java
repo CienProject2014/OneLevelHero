df3ab54b370c86f3ba1d07859891cf122192f93d
NakOh <kk0704@naver.com>
2015-09-29 20:08:03 +0900

    #512 [중요]ui_constants.json 수정 했으니까. 제발 합쳐줘

--- core/src/com/mygdx/unitStrategy/MonsterBattleStrategy.java
+++ core/src/com/mygdx/unitStrategy/MonsterBattleStrategy.java
@@ -29,8 +29,8 @@ public class MonsterBattleStrategy implements BattleStrategy {
 	@Override
 	public void attack(Unit attackMonster, Unit defender, int[][] hitArea) {
 		Buff overload = skillAssets.getBuff("overload");
-		int attackDmg = (int) attackMonster.getStatus().getAttack();
-		int defenseValue = (int) defender.getStatus().getDefense();
+		int attackDmg = (int) attackMonster.getRealStatus().getAttack();
+		int defenseValue = (int) defender.getRealStatus().getDefense();
 		int defenderHp = defender.getStatus().getHp();
 		int realDmg = attackDmg - defenseValue;
 		if (realDmg < 1) {
@@ -67,11 +67,6 @@ public class MonsterBattleStrategy implements BattleStrategy {
 	@Override
 	public void skill(Unit attackMonster, ArrayList<Unit> targetList, Skill skill) {
 		Gdx.app.log("Monster", attackMonster.getName() + "이(가) " + skill.getName() + "을(를) 사용하였습니다!");
-	}
-
-	@Override
-	public void runBuffEffect(Unit defender) {
-		applyBuff(defender);
 	}
 
 	private void applyBuff(Unit defender) {
@@ -111,7 +106,11 @@ public class MonsterBattleStrategy implements BattleStrategy {
 			case BLOCK_ACTION:
 				blockAction(defender);
 				break;
+			case INCREASE_AGGRO:
+				increaseAggro(defender);
+				break;
 			case DECREASE_ATTACK:
+				decreaseAttack(defender, buff);
 				break;
 			case DECREASE_HP_ITERATIVE:
 				decreaseHpIterative(defender, buff);
@@ -124,20 +123,149 @@ public class MonsterBattleStrategy implements BattleStrategy {
 			case DECREASE_DEFENSE:
 				decreaseDefense(defender, buff);
 				break;
+			case DECREASE_SPEED:
+				decreaseSpeed(defender, buff);
+				break;
+			case FLY_ACTION:
+				flyAction(defender);
+				break;
+			case OVERLOAD:
+				overload(defender);
+				break;
+			case OVERWORK:
+				overwork(defender);
+				break;
+			case SHOCK:
+				shock(defender);
+				break;
+			case WEAK:
+				weak(defender);
+				break;
+			case STINK:
+				stink(defender);
+				break;
+			case DECLINE:
+				decline(defender);
+				break;
+			case CHARM:
+				charm(defender);
+				break;
+			case INCREASE_FIRE_RESISTANCE:
+				increaseFireResistance(defender, buff);
+				break;
+			case INCREASE_WATER_RESISTANCE:
+				increaseWaterResistance(defender, buff);
+				break;
+			case INCREASE_ELECTRIC_RESISTANCE:
+				increaseElectricResistance(defender, buff);
+				break;
+			case BLESS:
+				bless(defender);
+				break;
+			case DEFAULT:
 			default:
 				break;
 			}
 		}
 	}
 
+	private void decreaseAttack(Unit defender, Buff buff) {
+		float preAttack = defender.getStatus().getAttack() * buff.getDecreaseAttackPercent() / 100;
+		defender.getRealStatus().setAttack(defender.getStatus().getAttack() - preAttack);
+
+	}
+
+	private void bless(Unit defender) {
+		float preMagicDefense = defender.getStatus().getAttack() * 15 / 100;
+		float preDefense = defender.getStatus().getDefense() * 15 / 100;
+		defender.getRealStatus().setMagicDefense(defender.getStatus().getMagicDefense() + preMagicDefense);
+		defender.getRealStatus().setDefense(defender.getStatus().getDefense() + preDefense);
+	}
+
+	private void increaseElectricResistance(Unit defender, Buff buff) {
+		int resistance = buff.getIncreaseElectricResistance();
+		defender.getRealStatus().setElectricResistance(defender.getStatus().getElectricResistance() + resistance);
+	}
+
+	private void increaseWaterResistance(Unit defender, Buff buff) {
+		int resistance = +buff.getIncreaseWaterResistance();
+		defender.getRealStatus().setWaterResistance(defender.getStatus().getWaterResistance() + resistance);
+	}
+
+	private void increaseFireResistance(Unit defender, Buff buff) {
+		int resistance = buff.getIncreaseFireResistance();
+		defender.getRealStatus().setFireResistance(defender.getStatus().getFireResistance() + resistance);
+	}
+
+	private void charm(Unit defender) {
+		float preAttack = defender.getStatus().getAttack() * 30 / 100;
+		float preMagicAttack = defender.getStatus().getMagicAttack() * 30 / 100;
+		defender.getRealStatus().setAttack(defender.getStatus().getAttack() - preAttack);
+		defender.getRealStatus().setMagicAttack(defender.getStatus().getMagicAttack() - preMagicAttack);
+	}
+
+	private void decline(Unit defender) {
+		float preMagicDefense = defender.getStatus().getMagicDefense() * 10 / 100;
+		float preDefense = defender.getStatus().getDefense() * 10 / 100;
+		defender.getRealStatus().setMagicDefense(defender.getStatus().getMagicDefense() - preMagicDefense);
+		defender.getRealStatus().setDefense(defender.getStatus().getDefense() - preDefense);
+	}
+
+	private void stink(Unit defender) {
+		float preAttack = defender.getStatus().getAttack() * 20 / 100;
+		float preDefense = defender.getStatus().getDefense() * 20 / 100;
+		defender.getRealStatus().setAttack(defender.getStatus().getAttack() - preAttack);
+		defender.getRealStatus().setDefense(defender.getStatus().getDefense() - preDefense);
+
+	}
+
+	private void weak(Unit defender) {
+		float preAttack = defender.getStatus().getAttack() * 10 / 100;
+		float preMagicAttack = defender.getStatus().getMagicAttack() * 10 / 100;
+		defender.getRealStatus().setAttack(defender.getStatus().getAttack() - preAttack);
+		defender.getRealStatus().setMagicAttack(defender.getStatus().getMagicAttack() - preMagicAttack);
+	}
+
+	private void shock(Unit defender) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void overwork(Unit defender) {
+		for (Hero hero : partyManager.getBattleMemberList()) {
+			float preAttack = hero.getStatus().getAttack() * 80 / 100;
+			float preDefense = hero.getStatus().getDefense() * 80 / 100;
+			hero.getRealStatus().setAttack(preAttack);
+			hero.getRealStatus().setDefense(preDefense);
+		}
+	}
+
+	private void overload(Unit defender) {
+	}
+
+	private void flyAction(Unit defender) {
+		defender.setAggro(0);
+	}
+
+	private void decreaseSpeed(Unit defender, Buff buff) {
+		int preSpeed = defender.getStatus().getSpeed() - 50;
+		defender.getRealStatus().setSpeed(preSpeed);
+	}
+
+	private void increaseAggro(Unit defender) {
+		int preAggro = defender.getAggro() + 900;
+		defender.setAggro(preAggro);
+	}
+
 	private void decreaseDefense(Unit defender, Buff buff) {
 		Hero hero = (Hero) defender;
-		defender.getStatus().setDefense(hero.getInventory().getAllDefense());
+		float preDefense = defender.getStatus().getDefense() - hero.getInventory().getAllDefense();
+		defender.getRealStatus().setDefense(preDefense);
 	}
 
 	private void increaseDefense(Unit defender, Buff buff) {
-		defender.getStatus()
-				.setDefense(defender.getStatus().getDefense() / 100 * (buff.getIncreaseDefensePercent() + 100));
+		float defense = defender.getStatus().getDefense() + buff.getIncreaseDefensePercent() / 100;
+		defender.getRealStatus().setDefense(defender.getStatus().getDefense() + defense);
 	}
 
 	private void blockAction(Unit defender) {
@@ -151,5 +279,10 @@ public class MonsterBattleStrategy implements BattleStrategy {
 				* (buff.getDecreaseHpAttackPercent() / 100.0f);
 		float delta = factor * (attackerMagicDamage + attackerAttackDamage) + buff.getDecreaseHpOffset();
 		defender.getStatus().setHp((int) (defender.getStatus().getHp() - delta));
+	}
+
+	@Override
+	public void runBuffEffect(Unit defender) {
+		applyBuff(defender);
 	}
 }
