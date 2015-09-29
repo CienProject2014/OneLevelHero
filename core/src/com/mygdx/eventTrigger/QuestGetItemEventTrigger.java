123fb877d161a9790af97d8b80dee9e8e83bd7e3
HoraceVelmont <dragond7@naver.com>
2015-09-29 14:48:05 +0900

    #544 팅김 방지를위해 퀘스트 이벤트 만듦

---
+++ core/src/com/mygdx/eventTrigger/QuestGetItemEventTrigger.java
@@ -0,0 +1,30 @@
+package com.mygdx.eventTrigger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.mygdx.enums.QuestEnum.QuestState;
+import com.mygdx.enums.QuestEnum.QuestType;
+import com.mygdx.manager.QuestManager;
+import com.mygdx.manager.StorySectionManager;
+import com.mygdx.model.event.EventParameters;
+import com.mygdx.model.event.Quest;
+
+public class QuestGetItemEventTrigger implements EventTrigger {
+	@Autowired
+	private QuestManager questManager;
+	@Autowired
+	private StorySectionManager storySectionManager;
+
+	@Override
+	public void triggerEvent(EventParameters eventParameter) {
+		Quest quest = new Quest();
+		quest.setQuestName(eventParameter.getQuest().getQuestName());
+		quest.setQuestObjectPath(eventParameter.getQuest().getQuestObjectPath());
+		quest.setQuestObjectCount(eventParameter.getQuest().getQuestObjectCount());
+		quest.setQuestType(QuestType.GET_ITEM);
+		quest.setQuestState(QuestState.ING);
+		questManager.addQuest(quest);
+		storySectionManager.runStorySequence();
+	}
+
+}
