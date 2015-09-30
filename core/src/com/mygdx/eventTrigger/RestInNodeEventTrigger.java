89b2d48751781fd990d4147beb85942f65fadf38
HoraceVelmont <dragond7@naver.com>
2015-09-30 19:18:42 +0900

    #569 잡다한 내용 수정

--- core/src/com/mygdx/eventTrigger/RestInNodeEventTrigger.java
+++ core/src/com/mygdx/eventTrigger/RestInNodeEventTrigger.java
@@ -3,16 +3,20 @@ package com.mygdx.eventTrigger;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.mygdx.manager.PartyManager;
+import com.mygdx.manager.TimeManager;
 import com.mygdx.model.event.EventParameters;
 
 public class RestInNodeEventTrigger implements EventTrigger {
 	@Autowired
 	private PartyManager partyManager;
+	@Autowired
+	private TimeManager timeManager;
 
 	@Override
 	public void triggerEvent(EventParameters eventParameter) {
 		partyManager.healAllHero();
 		partyManager.setFatigue(0);
+		timeManager.plusHour(4);
 	}
 
 }
